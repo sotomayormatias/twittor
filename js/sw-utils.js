7c9ece27442555d43bf63100116ff827da57cabd
msotomayor <msotomayor@tgv.com.ar>
2021-02-22 19:12:03 -0300

    Initial commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+
+const updateDynamicCache = (dynamicCache, req, res) => {
+    if (res.ok) {
+        return caches.open(dynamicCache)
+            .then(cache => {
+                cache.put(req, res.clone());
+                return res.clone();
+            });
+    } else {
+        return res;
+    }
+}
