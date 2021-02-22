75894418c4db0a3a2af3f0e461ea0d09b100a698
msotomayor <msotomayor@tgv.com.ar>
2021-02-22 20:19:31 -0300

    fixs for accesibility

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -39,15 +39,25 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-    const response = caches.keys().then(keys => {
+
+    const respuesta = caches.keys().then(keys => {
+
         keys.forEach(key => {
+
             if (key !== STATIC_CACHE && key.includes('static')) {
                 return caches.delete(key);
             }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+                return caches.delete(key);
+            }
+
         });
+
     });
 
-    e.waitUntil(response);
+    e.waitUntil(respuesta);
+
 });
 
 self.addEventListener('fetch', e => {
